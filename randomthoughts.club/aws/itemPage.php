426edb3301881baaa32dc44657975f5a4db4389f
AdamCox9 <adam.cox9@gmail.com>
2017-10-04 08:02:53 -0400

    cleaned up directory a bit

--- randomthoughts.club/aws/itemPage.php
+++
@@ -1,6 +0,0 @@
-<?PHP
-
-	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: http://www.8d8apps.com/aws/itemPage.php?item={$_GET['item']}");
-
-?>
--- randomthoughts.club/aws/storeFront.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-
-	$pagenum = isset( $_GET['pagenum'] ) ? $_GET['pagenum'] : 1;
-	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: http://www.8d8apps.com/aws/storeFront.php?pagenum=$pagenum&category={$_GET['category']}&displayCat={$_GET['displayCat']}");
-
-?>
